bf2ed5436cbaafad83b4ed3a673b0ae5560a0e97
Sheza Shamsher <160360815+ShezaShamsher@users.noreply.github.com>
2024-03-21 05:56:06 -0700

    My first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "first number",
+        type: "number",
+        name: "num1",
+    },
+    {
+        message: "Second number",
+        type: "number",
+        name: "num2",
+    },
+    {
+        message: "select one of the operator",
+        type: "list",
+        name: "operator",
+        choices: [
+            "Addition",
+            "Subtraction",
+            "Multiplication",
+            "Division",
+            "modulus",
+            "Expontention",
+        ],
+    },
+]);
+// Condition statement
+if (answer.operator === "Addition") {
+    console.log("Your value is", answer.num1 + answer.num2);
+}
+else if (answer.operator === "Subtraction") {
+    console.log("Your value is", answer.num1 - answer.num2);
+}
+else if (answer.operator === "Multiplication") {
+    console.log("Your value is", answer.num1 * answer.num2);
+}
+else if (answer.operator === "Division") {
+    console.log("Your value is", answer.num1 / answer.num2);
+}
+else if (answer.operator === "modulus") {
+    console.log("Your value is", answer.num1 % answer.num2);
+}
+else if (answer.operator === "Expontention") {
+    console.log("Your value is", answer.num1 ** answer.num2);
+}
+else {
+    console.log("Invalid operator");
+}
