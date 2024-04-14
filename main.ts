1192c25429cdd324971c340bd69f39958751e06a
Sheza Shamsher <160360815+ShezaShamsher@users.noreply.github.com>
2024-04-14 07:39:55 -0700

    upated

--- main.ts
+++ main.ts
@@ -1,19 +1,22 @@
 #! /usr/bin/env node 
 
 import inquirer from "inquirer";
+import chalk from "chalk" ;
+console.log(chalk.underline.magentaBright(`\t\n "Welcome to codewithsheza - simple calculator"\n`));
+
 const answer = await inquirer.prompt([
     {
-      message: "first number",
+      message: chalk.yellow.italic("first number"),
       type: "number",
       name: "num1",
     },
     {
-      message: "Second number",
+      message: chalk.yellow.italic("Second number"),
       type: "number",
       name: "num2",
     },
     {
-      message: "select one of the operator",
+      message: chalk.yellow.italic("select one of the operator"),
       type: "list",
       name: "operator",
       choices: [
@@ -28,18 +31,18 @@ const answer = await inquirer.prompt([
   ]);
   // Condition statement
   if (answer.operator === "Addition") {
-    console.log("Your value is", answer.num1 + answer.num2);
+    console.log(chalk.redBright.bold.underline("\t\nYour value is", answer.num1 + answer.num2));
   } else if (answer.operator === "Subtraction") {
-    console.log("Your value is", answer.num1 - answer.num2);
+    console.log(chalk.redBright.bold.underline("\t\nYour value is", answer.num1 - answer.num2));
   } else if (answer.operator === "Multiplication") {
-    console.log("Your value is", answer.num1 * answer.num2);
+    console.log(chalk.redBright.bold.underline("\t\nYour value is", answer.num1 * answer.num2));
   } else if (answer.operator === "Division") {
-    console.log("Your value is", answer.num1 / answer.num2);
+    console.log(chalk.redBright.bold.underline("\t\nYour value is", answer.num1 / answer.num2));
   } else if (answer.operator === "modulus") {
-    console.log("Your value is", answer.num1 % answer.num2);
+    console.log(chalk.redBright.bold.underline("\t\nYour value is", answer.num1 % answer.num2));
   } else if (answer.operator === "Expontention") {
-    console.log("Your value is", answer.num1 ** answer.num2);
+    console.log(chalk.redBright.bold.underline("\t\nYour value is", answer.num1 ** answer.num2));
   } else {
-    console.log("Invalid operator");
+    console.log(chalk.blueBright.bold("Invalid operator"));
   }
   
